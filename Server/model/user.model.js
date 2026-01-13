dee5f67842a53b5e3dea973f470e26eb4fd99b40
Divyam Tiwari <tiwarixdivyam@gmail.com>
2026-01-13 21:11:59 +0530

    user model done

--- Server/model/user.model.js
+++ Server/model/user.model.js
@@ -1,2 +1,34 @@
-import mongoose, {Schema} from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
+const userSchema = Schema.create({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 3,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 3,
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    trim: true,
+    unique: true,
+    index: true,
+    match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "Please provide a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "password is required!"],
+    trim: true,
+    select: false,
+    minlength: 8,
+  },
+});
+
+export const User = mongoose.model("User", userSchema);
